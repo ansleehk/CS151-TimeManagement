007a3bc43852e4a5ae9dd10072b733651e13b82a
leeanson19 <leeanson19@gmail.com>
2023-04-19 19:37:20 -0700

    Add event showing func in calendar

--- frontend/src/pages/notFound.jsx
+++ frontend/src/pages/notFound.jsx
@@ -1,7 +1,13 @@
+import studentStressedOutImg from "./img/student_stressed_out.png"
+import "./styles/notFound.scss"
+
 export default function Error404 (){
     return (
-        <div>
-            Error 404
+        <div id="not-found-container">
+            <h1>
+                Page Not Found
+            </h1>
+            <img src={studentStressedOutImg} />
         </div>
     )
 }
