8225454b4ed5d9223cbf368e0411353f61d6e418
bfiloteo <83046619+bfiloteo@users.noreply.github.com>
2023-05-01 22:43:45 -0700

    Update login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,66 +1,47 @@
 import React, { useState } from 'react'
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from 'mdbreact';
-import '../styles/loginForm.css';
-import timePicture from '../components/img/timePicture.png';
+import "./styles/login.css"
 
-//loginForm function, takes in login and error props from loginPage.js
-export function LoginPage() {
+import timePicture from './img/timePicture.png';
+
+export default function Login() {
     return (
-		<div className='registerPage'>
+      
+      
+      <container >
 
-        <div className="form-inner">
-            <MDBRow className='d-flex justify-content-center align-items-center h-50'>
-              <MDBCol col='6'>
+      <ul class='loginstack'>
+  
+        <div id="RegisterPage" className='page-container right-page-container form-container'>
+          <h1 class="LoginTitle">Login</h1>
+  
+          <p class="subtitle">
+                  Login To Your Account!
+              </p>
+          <form >
+  
+          <container class="Info" >
+  
+            <div>
+              <input type="email" placeholder='Enter Email' class='emailinput h'/>
+            </div>
+  
+            <div>
+              <input type="password" placeholder='Enter Password' class='passwordinput h' />
+            </div>
 
-                <MDBCard className='registerCard my-5 mx-lg-5' style={{ borderRadius: '1rem', maxWidth: '500px' }}>
-                <MDBCardBody className='p-5 d-flex flex-column align-items-center w-100'>
-
-                    <h2 className="fw-bold mb-2 text-uppercase">Create an Account</h2>
-                    <center><p className="mb-10">Time Management is the key to success</p></center>
-
-                    <div className='d-flex flex-row justify-content-center'>
-                      <form>
-                        <div classname="form-group">
-                          <input type="Email" name="email" id="email" placeholder="Email" />
-                          <p className="small mb-3"></p>
-                        </div>
-                        <div classname="form-group">
-                          <input type="password" name="password" id="password" placeholder="Password" />
-                          <p className="small mb-3"></p>
-                        </div>
-                        <div classname="form-group">
-						  <div className='loginFooter'>
-											<p className="mb-0">Don't have an account? <a href="./register" class="text-white-50 fw-bold">Sign Up</a></p>
-										</div>
-                        </div>
-                      </form>
-                    </div>
-
-                    <button className="submitB" type='submit'>Create Account</button>
-                    <div className='d-flex flex-row mt-3'>
-                      <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                        <MDBIcon fab icon='facebook-f' size="lg" />
-                      </MDBBtn>
-
-                      <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                        <MDBIcon fab icon='twitter' size="lg" />
-                      </MDBBtn>
-
-                      <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                        <MDBIcon fab icon='google' size="lg" />
-                      </MDBBtn>
-                    </div>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-
-              <MDBCol col='6' className='my-5 mx-lg-5' style={{ maxWidth: '580px', }}>
-                <div className="timePicture">
-                  <img src={timePicture} width="200px" height="auto"></img>
-                </div>
-              </MDBCol>
-            </MDBRow>
-        </div>
-      </div>
-    );
-  }
+  
+            <button class="loginbutton" type='submit'>Login</button>
+  
+          </container>
+  
+  
+          </form>
+  
+  
+          </div>
+          
+  
+        </ul>
+        </container>
+      );
+}
