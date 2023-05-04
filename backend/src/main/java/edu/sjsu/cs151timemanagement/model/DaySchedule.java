832ad9ae26fefef960490df4194833ca2ab2b874
bfiloteo <83046619+bfiloteo@users.noreply.github.com>
2023-05-03 23:52:34 -0700

    Update DaySchedule.java

--- backend/src/main/java/edu/sjsu/cs151timemanagement/model/DaySchedule.java
+++ backend/src/main/java/edu/sjsu/cs151timemanagement/model/DaySchedule.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DaySchedule {
+    
+    // Each schedule will be a HashMap where the key is the date of activities, and value is an arraylist of both events and routines.
     private HashMap<LocalDate, ArrayList<ScheduledDayActivity>> schedules;
 
     public DaySchedule() {
